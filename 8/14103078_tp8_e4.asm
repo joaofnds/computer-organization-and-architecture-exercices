60d76652c3e78774b515557e17fd3cdf78e1ac2e
João Fernandes <joaovitorfernandes2@gmail.com>
2018-11-07 01:20:01 -0200

    Enhance insertion sort

--- 8/14103078_tp8_e4.asm
+++ 8/14103078_tp8_e4.asm
@@ -1,11 +1,10 @@
 .data
-  vector: .word 3, 7, 4, 9, 5, 2, 6
-  length: .word 7
+  vector: .word 3, 8, 7, 4, 9, 5, 2, 6, 1
 
 .text
 main:
   la $a0, vector # load vector address
-  lw $a1, length # load vector length
+  li $a1, 8      # last index
 
   jal insertion_sort
   nop
@@ -14,31 +13,31 @@ main:
   syscall
 
 insertion_sort:
-  li $t0, 1 # counter
-  or $t5, $0, $a0 # *index pointer
+  or  $t0, $0,  $a0 # index pointer
+  sll $t1, $a1, 2   # multiply by 4 to get length in bits
+  add $t1, $t0, $t1 # vector end pointer
 
   walk_loop:
-    beq $t0, $a1, return # if counter > length(vector), return
+    beq $t0, $t1, return # if index_pointer > vector_end_pointer, return
     nop
 
-		addi $t0, $t0, 1  # counter++
-    addi, $t5, $t5, 4 # (*index)++
-    or $t1, $0, $t5 # pivot = index
+    addi $t0, $t0, 4   # index++
+    or   $t2, $0,  $t0 # pivot = index
 
     swap_loop:
-      beq $t1, $a0, walk_loop # if pivot = arr
+      beq $t2, $a0, walk_loop # if pivot = vector
       nop
 
-      lw  $t3, ($t1)   # $t3 = *pivot
-      lw  $t4, -4($t1) # $t4 = *(pivot-1)
+      lw  $t3, ($t2)   # $t3 = *pivot
+      lw  $t4, -4($t2) # $t4 = *(pivot-1)
 
       bgt $t3, $t4, walk_loop
       nop
 
-      sw $t4, ($t1)
-      sw $t3, -4($t1)
+      sw $t4, ($t2)
+      sw $t3, -4($t2)
 
-      sub $t1, $t1, 4
+      sub $t2, $t2, 4
 
       j swap_loop
       nop
