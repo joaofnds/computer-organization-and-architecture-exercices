3e6a3fd713a4ac3e4264ce5c44ded0fcac152f27
João Fernandes <joaovitorfernandes2@gmail.com>
2018-11-18 19:47:53 -0200

    Fix typos in tp8 e2

--- 8/14103078_tp8_e2.asm
+++ 8/14103078_tp8_e2.asm
@@ -6,7 +6,7 @@
 .text
 
 main:
-		lui $s0, 0x1001 # start os data memory
+		lui $s0, 0x1001 # start of data memory
 		li  $s1, 10     # 10 used as a constant
 		li  $s2, 1      # decimal place multiplier
 		li  $s3, 0      # result acc
@@ -27,14 +27,14 @@ loop:
 		nop
 		bgt $t1, 57, continue # continue if char > '9'
 		nop
-		
+
 		subi $t1, $t1, 48     # 48 = ASCII code for '0'
-		
+
 		mult $t1, $s2         # number *= decimal_acc
 		mflo $t1
 		mult $s2, $s1         # decimal_acc *= 10
 		mflo $s2
-		
+
 		add $s3, $s3, $t1     # acc += new_number
 continue:
 		subi $t0, $t0, 1     # read next chart to the left
@@ -42,11 +42,11 @@ continue:
 		j loop
 		nop
 
-exit:		
+exit:
 		li $v0, 1       # print result
 		or $a0, $0, $s3
 		syscall
-		
+
 		or $t0, $0, $s3 # store result in $t0
 
 		li $v0, 0xa
